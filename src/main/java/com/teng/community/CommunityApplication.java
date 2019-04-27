de9c6b9ae33ff748c7e770ba07f212f9d8f157a5
zhouteng <wwwsousoucom@126.com>
2019-04-27 16:38:11 +0800

    ok

---
+++ src/main/java/com/teng/community/CommunityApplication.java
@@ -0,0 +1,17 @@
+package com.teng.community;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/*
+	用spring 注解 管理bean
+ */
+
+@SpringBootApplication
+public class CommunityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommunityApplication.class, args);
+	}
+
+}
